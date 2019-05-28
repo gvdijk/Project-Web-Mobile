907a51e65a887f24fc7a44eae5aa51a13e936ab0
Vincent Cremers <vincent.cremers@live.nl>
2019-05-28 15:40:02 +0200

    flask testing

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from data import Posts
 
 app = Flask(__name__)
 
+posts = Posts()
+
+@app.route('/')
+def index():
+    return render_template('home.html', posts = posts)
+
+@app.route('/post/<string:id>')
+def post(id):
+    return render_template('post.html', id = id)
 
 if(__name__ == '__main__'):
-    app.run()
+    #TODO disable debugmode
+    app.run(debug=True)
