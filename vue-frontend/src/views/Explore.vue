5c550cad0f4b6878acf99007dc6863c2aa18f303
Justin Zuidgeest <justinzuidgeest@gmail.com>
2019-06-09 19:45:19 +0200

    Fixed explore page to get data from API

--- vue-frontend/src/views/Explore.vue
+++ vue-frontend/src/views/Explore.vue
@@ -1,48 +1,52 @@
 <template>
     <div class="home">
         <div class="projects-view">
-            <ProjectTile v-for="project in projects" :key="project.id" v-bind:project="project" />
+            <ProjectTile v-for="project in paginatedProjects" :key="project.id" v-bind:project="project" />
         </div>
+    <PageSelector v-bind:visiblePages="3" v-bind:totalEntries="projects.length" v-bind:entriesPerPage="5" v-on:pageChanged="pageChanged"/>
     </div>
 </template>
 
 <script>
 import ProjectTile from '../components/ProjectTile.vue'
+import PageSelector from '../components/PageSelector.vue'
+
 export default {
     name: 'Explore',
     components: {
-        ProjectTile
+        ProjectTile,
+        PageSelector,
     },
     data() {
         return {
-            projects: [
-                {
-                    id: 1,
-                    title: 'Invisiline',
-                    description: 'I have taken out my invisiline, and this is the album',
-                    users: 34,
-                    access: true,
-                    created: 1539550165
-                },
-                {
-                    id: 2,
-                    title: 'Barbarapapa',
-                    description: 'Vrouger hadden we nog echte TV series',
-                    users: 2,
-                    access: true,
-                    created: 1549558165
-                },
-                {
-                    id: 3,
-                    title: 'De beste titel voor een lange beschrijving',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                    users: 1,
-                    access: false,
-                    created: 1449558165
-                },
-            ]
+            projects: [],
+            startIndex: 0,
+            endIndex: 0
         }
     },
+    methods: {
+        fetchPosts(){
+            this.$store.dispatch('getProjects')
+            .then( response => {
+                this.projects = response;
+            })
+            .catch( error => {
+                console.log(error);
+            })
+        },
+        pageChanged(startIndex, endIndex){
+            this.startIndex = startIndex;
+            this.endIndex = endIndex;
+        },
+    },
+    computed: {
+        paginatedProjects(){
+            return this.projects.slice(this.startIndex, this.endIndex);
+        },
+    },
+    created(){
+        this.fetchPosts();
+    }
 }
 </script>
 
