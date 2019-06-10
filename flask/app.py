0c7295cdef325f2b837b705e05f74ea090da6e51
Vincent Cremers <vincent.cremers@live.nl>
2019-06-10 13:43:22 +0200

    db connection + user api

--- flask/app.py
+++ flask/app.py
@@ -1,18 +1,50 @@
-from flask import Flask, render_template
-from data import Posts
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+import datetime
 
 app = Flask(__name__)
 
-posts = Posts()
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'admin' # Super encripted and safe password usage 
+app.config['MYSQL_DB'] = 'mydb'
 
-@app.route('/')
+mysql = MySQL(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('home.html', posts = posts)
+    if request.method == 'POST':
+        # Fetch form data
+        userDetails = request.form
+        name = userDetails['name']
+        # id = userDetails['id']
+        password = userDetails['password']
+        picture = userDetails['picture']
+        created = datetime.date.today()
 
-@app.route('/post/<string:id>')
-def post(id):
-    return render_template('post.html', id = id)
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO user(userName, userPass, userPicture ,userCreated) VALUES(%s, %s, %s, %s)", (name, password, picture, created))
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/users')
+    return render_template('index.html')
+
+@app.route('/users')
+def users():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM user")
+    if resultValue > 0:
+        userDetails = cur.fetchall()
+        return render_template('users.html',userDetails=userDetails)
+
+@app.route('/user/<string:id>')
+def get_user(id):
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM user WHERE userID = " + id)
+    if resultValue > 0:
+        userDetails = cur.fetchall()
+        return render_template('users.html',userDetails=userDetails)
 
 if(__name__ == '__main__'):
-    #TODO disable debugmode
     app.run(debug=True)
