585343543e601aab9699315de32bb34616241ea8
Gerard van Dijk <gerardvandijk2@gmail.com>
2019-07-04 00:07:07 +0200

    Vue PWA init

---
+++ vue-frontend/dist/precache-manifest.96a23e104ddf37e7436dcbddac2adae2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "da6bfcdc6cff6e58d366",
+    "url": "/js/chunk-vendors.b81f6921.js"
+  },
+  {
+    "revision": "7d51a93f58ef26644826",
+    "url": "/js/app.de56e730.js"
+  },
+  {
+    "revision": "a4a258d60a26d537d3f0",
+    "url": "/js/about.d58aa3ee.js"
+  },
+  {
+    "revision": "acbf9489a327eb6cd821327fc4c79a35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8facd64e86021c6aafd63632d0108d19",
+    "url": "/img/Logo_Full_White.8facd64e.svg"
+  },
+  {
+    "revision": "7d51a93f58ef26644826",
+    "url": "/css/app.2c355c44.css"
+  },
+  {
+    "revision": "a4a258d60a26d537d3f0",
+    "url": "/css/about.3bbed927.css"
+  }
+];
